474971c5a62b63f88ee5be5a33571637927bb691
Daniel Nebatz <dnebatz@student.42wolfsburg.de>
2023-07-24 20:46:00 +0200

    sprite movement added

--- source/main.c
+++ source/main.c
@@ -6,11 +6,10 @@
 /*   By: dnebatz <dnebatz@student.42wolfsburg.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/20 11:45:21 by dnebatz           #+#    #+#             */
-/*   Updated: 2023/07/24 19:37:15 by dnebatz          ###   ########.fr       */
+/*   Updated: 2023/07/24 20:13:53 by dnebatz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <mlx.h>
 #include "../includes/so_long.h"
 
 t_sprite	*ft_lstimgnew(char *content)
@@ -49,26 +48,10 @@ void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 	*(unsigned int *)dst = color;
 }
 
-int	color_map_1(void *mlx, void *win, int w, int h)
+int	ft_move_right(t_init *init, t_sprite *ghost, int *posx, int *posy)
 {
-	int	x;
-	int	y;
-	int	color;
-
-	x = w;
-	while (x--)
-	{
-		y = h;
-		while (y--)
-		{
-			//color = (x * 255) / w + ((((w - x) * 255) / w) << 16) + (( (y * 255) / h) << 8);
-			if (x < 200 && y < 200)
-				color = 12;
-			else
-				color = 122;
-			mlx_pixel_put(mlx, win, x, y, color);
-		}
-	}
+	*posx += 48;
+	mlx_put_image_to_window(init->mlx, init->win, ghost->img, *posx, *posy);
 	return (0);
 }
 
@@ -81,7 +64,7 @@ int	key_hook(int keycode, t_init *init)
 	else if (keycode == KEY_LEFT || keycode == KEY_A)
 		printf("walk left!\n");
 	else if (keycode == KEY_RIGHT || keycode == KEY_D)
-		printf("walk right!\n");
+		ft_move_right(init, init->sprite, &init->sprite->posx, &init->sprite->posy);
 	else if (keycode == KEY_ESC || keycode == KEY_Q)
 	{
 		mlx_destroy_window(init->mlx, init->win);
@@ -92,7 +75,6 @@ int	key_hook(int keycode, t_init *init)
 	return (0);
 }
 
-
 int	main(void)
 {
 	void		*mlx;
@@ -100,9 +82,10 @@ int	main(void)
 	void		*img;
 	t_sprite	*ghost;
 	t_init		*init;
-	// char		*spritepath = "./sprites/ghost.xpm";
-	// ghost = ft_lstimgnew(spritepath);
+
 	ghost = ft_lstimgnew("./sprites/ghost.xpm");
+	ghost->posx = 48;
+	ghost->posy = 48;
 	mlx = mlx_init();
 	if (!mlx)
 	{
@@ -110,24 +93,23 @@ int	main(void)
 		return (0);
 	}
 	init = ft_lstinitnew(mlx);
-	win = mlx_new_window(mlx, WIN_X, WIN_Y, "so_long");
-	init->win = win;
-	if (!win)
+	init->win = mlx_new_window(mlx, WIN_X, WIN_Y, "so_long");
+	//init->win = win;
+	if (!init->win)
 	{
 		ft_printf("win creation error!\n");
 		return (0);
 	}
-	img = mlx_xpm_file_to_image(mlx, ghost->relative_path, &ghost->width, &ghost->height);
-	ghost->img = img;
-	if (!img)
+	ghost->img = mlx_xpm_file_to_image(mlx, ghost->relative_path, &ghost->width, &ghost->height);
+	if (!ghost->img)
 	{
 		ft_printf("sprite creation error!\n");
 		return (0);
 	}
-
+	init->sprite = ghost;
 	mlx_put_image_to_window(init->mlx, init->win, ghost->img, 0, 0);
 	mlx_put_image_to_window(init->mlx, init->win, ghost->img, 100, 100);
-    mlx_hook(init->win, 2, 1L<<00, key_hook, init);
+	mlx_hook(init->win, 2, 1L << 00, key_hook, init);
 	mlx_loop(init->mlx);
 	return (0);
 }
