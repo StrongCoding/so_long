490fd9791abff037e9811214a63321edb8ab5b88
Daniel Nebatz <dnebatz@student.42wolfsburg.de>
2023-07-25 10:47:51 +0200

    added line counting function

--- source/ft_parse_map.c
+++ source/ft_parse_map.c
@@ -6,24 +6,70 @@
 /*   By: dnebatz <dnebatz@student.42wolfsburg.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/24 21:09:00 by dnebatz           #+#    #+#             */
-/*   Updated: 2023/07/24 21:44:22 by dnebatz          ###   ########.fr       */
+/*   Updated: 2023/07/25 10:47:03 by dnebatz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/so_long.h"
 
-char	**ft_parse_map(char *file)
+char	**ft_parse_map(char *file, int rows)
+{
+	int		fd;
+	char	*row;
+	char	**map;
+	int		i;
+
+	i = -1;
+	fd = open(file, O_RDONLY);
+	if (fd < 1)
+	{
+		ft_printf("Error file!\n");
+		return (NULL);
+	}
+	map = malloc((rows + 1) * sizeof(char *));
+	while (++i <= rows)
+	{
+		map[i] = get_next_line(fd);
+		printf("map[%i]:%s\n", i, map[i]);
+	}
+	close(fd);
+	return (map);
+}
+
+int	ft_get_rows(char *file)
 {
 	int		fd;
 	int		count_rows;
-	t_list	*rows;
 	char	*row;
 
+	count_rows = 0;
 	fd = open(file, O_RDONLY);
-	if (!fd)
-		return (NULL);
-	rows = ft_lstnew(get_next_line(fd));
-	while (ft_lstadd_back(&rows, ft_lstnew(get_next_line(fd))))
+	if (fd < 1)
+	{
+		ft_printf("Error file!\n");
+		return (-1);
+	}
+	row = get_next_line(fd);
+	while (!(row == NULL))
+	{
 		count_rows++;
-	printf("content %s\n", rows->content);
+		free(row);
+		row = get_next_line(fd);
+	}
+	free(row);
+	close(fd);
+	return (count_rows);
+}
+
+int	main(void)
+{
+	int		i;
+	char	**string;
+
+	i = ft_get_rows("../maps/map1.ber");
+	printf("count rows:%i\n", i);
+	string = ft_parse_map("../maps/map1.ber", i);
+	i = -1;
+	while(string[++i])
+		printf("string[%i]: %s\n", i, string[i]);
 }
