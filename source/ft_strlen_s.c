8e70b74d862fd734ab1afff7b198b7932f798730
Daniel Nebatz <dnebatz@student.42wolfsburg.de>
2023-07-25 21:41:00 +0200

    added discription

--- source/ft_strlen_s.c
+++ source/ft_strlen_s.c
@@ -6,12 +6,13 @@
 /*   By: dnebatz <dnebatz@student.42wolfsburg.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/17 10:06:25 by dnebatz           #+#    #+#             */
-/*   Updated: 2023/07/25 16:47:24 by dnebatz          ###   ########.fr       */
+/*   Updated: 2023/07/25 21:40:51 by dnebatz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/so_long.h"
 
+//return size until 0 or \n
 size_t	ft_strlen_s(const char *s)
 {
 	unsigned long	i;
