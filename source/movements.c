e61e39868969a3f1217ad6aac33c4a3efbe6873d
Daniel Nebatz <dnebatz@student.42wolfsburg.de>
2023-07-26 17:19:46 +0200

    added the changing of map too

--- source/movements.c
+++ source/movements.c
@@ -6,7 +6,7 @@
 /*   By: dnebatz <dnebatz@student.42wolfsburg.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/26 09:48:53 by dnebatz           #+#    #+#             */
-/*   Updated: 2023/07/26 10:28:27 by dnebatz          ###   ########.fr       */
+/*   Updated: 2023/07/26 11:50:06 by dnebatz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,9 @@ int	ft_move_right(t_init *init)
 		return (0);
 	mlx_put_image_to_window(init->mlx, init->win,
 		init->ground->img, init->x, init->y);
+	init->map[init->y / 48][init->x / 48] = GROUND;
 	init->x += 48;
+	init->map[init->y / 48][init->x / 48] = GHOST;
 	mlx_put_image_to_window(init->mlx, init->win,
 		init->ghost->img, init->x, init->y);
 	return (0);
@@ -30,7 +32,9 @@ int	ft_move_left(t_init *init)
 		return (0);
 	mlx_put_image_to_window(init->mlx, init->win,
 		init->ground->img, init->x, init->y);
+	init->map[init->y / 48][init->x / 48] = GROUND;
 	init->x -= 48;
+	init->map[init->y / 48][init->x / 48] = GHOST;
 	mlx_put_image_to_window(init->mlx, init->win,
 		init->ghost->img, init->x, init->y);
 	return (0);
@@ -42,7 +46,9 @@ int	ft_move_up(t_init *init)
 		return (0);
 	mlx_put_image_to_window(init->mlx, init->win,
 		init->ground->img, init->x, init->y);
+	init->map[init->y / 48][init->x / 48] = GROUND;
 	init->y -= 48;
+	init->map[init->y / 48][init->x / 48] = GHOST;
 	mlx_put_image_to_window(init->mlx, init->win,
 		init->ghost->img, init->x, init->y);
 	return (0);
@@ -54,7 +60,9 @@ int	ft_move_down(t_init *init)
 		return (0);
 	mlx_put_image_to_window(init->mlx, init->win,
 		init->ground->img, init->x, init->y);
+	init->map[init->y / 48][init->x / 48] = GROUND;
 	init->y += 48;
+	init->map[init->y / 48][init->x / 48] = GHOST;
 	mlx_put_image_to_window(init->mlx, init->win,
 		init->ghost->img, init->x, init->y);
 	return (0);
