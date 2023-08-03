c30e361d55ab1c310aa8f8a76e390e84d17ff8d6
Daniel Nebatz <dnebatz@student.42wolfsburg.de>
2023-08-03 12:15:38 +0200

    added error message

--- source/ft_check_argv.c
+++ source/ft_check_argv.c
@@ -6,7 +6,7 @@
 /*   By: dnebatz <dnebatz@student.42wolfsburg.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/26 17:33:08 by dnebatz           #+#    #+#             */
-/*   Updated: 2023/07/26 18:00:32 by dnebatz          ###   ########.fr       */
+/*   Updated: 2023/08/03 12:15:00 by dnebatz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,14 @@ int	ft_check_argv(char **argv)
 	while (argv[1][i])
 		i++;
 	if (i <= 4)
+	{
+		ft_printf("Error\nwrong file format need to be filename.ber\n");
 		return (-1);
+	}
 	if (ft_strncmp(&argv[1][i - 4], ".ber", 5))
+	{
+		ft_printf("Error\nwrong file format need to be filename.ber\n");
 		return (-1);
+	}
 	return (1);
 }
